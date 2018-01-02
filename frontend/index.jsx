70339355cc528e54d6ff36fd04aafe0d3ed8e703
monicakornis <2011mkornis@gmail.com>
2018-01-02 18:07:12 -0500

    adding store

--- frontend/index.jsx
+++ frontend/index.jsx
@@ -1,12 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import * as SessionApiUtil from './util/session_api_util';
+import configureStore from './store/store.js';
 
 window.login = SessionApiUtil.login;
 window.logout = SessionApiUtil.logout;
 window.signup = SessionApiUtil.signup;
 
+
 document.addEventListener('DOMContentLoaded',() => {
   const root = document.getElementById('root');
+  const store = configureStore();
+  window.getState = store.getState;
+  window.dispatch = store.dispatch; 
   ReactDOM.render(<h1>Welcome to Delish!</h1>,root);
 });
---
+++ frontend/reducers/root_reducer.js
@@ -0,0 +1,10 @@
+import { combineReducers } from 'react-redux';
+import sessionsReducer from './session_reducer';
+import errorsReducer from './errors_reducer';
+
+const rootReducer = combineReducers({
+    errors: errorsReducer,
+    session: sessionsReducer
+});
+
+export default rootReducer; 
