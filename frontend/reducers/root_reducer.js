b5e57977451e1cc30b476d0a2586ef9c9b2ba71d
monicakornis <2011mkornis@gmail.com>
2018-01-02 17:51:11 -0500

    removed changes to root reducer

--- frontend/reducers/root_reducer.js
+++ frontend/reducers/root_reducer.js
@@ -1,10 +1,1 @@
-import { combineReducers } from 'react-redux';
-import sessionsReducer from './session_reducer';
-import errorsReducer from './errors_reducer';
 
-const rootReducer = combineReducers({
-  return {
-    errors: errorsReducer,
-    session: session
-  }
-})
