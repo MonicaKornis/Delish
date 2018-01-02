2d7fdba0f629bdb47ac48cef9f416bf19eac3d39
monicakornis <2011mkornis@gmail.com>
2018-01-02 12:01:28 -0500

    finished setup

--- frontend/index.jsx
+++ frontend/index.jsx
@@ -3,5 +3,5 @@ import ReactDOM from 'react-dom';
 
 document.addEventListener('DOMContentLoaded',() => {
   const root = document.getElementById('root');
-  ReactDOM.render(<h1>Delish</h1>,root);
+  ReactDOM.render(<h1>Welcome to Delish!</h1>,root);
 });
