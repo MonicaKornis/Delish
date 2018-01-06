f9552d8cb8d37aceb379ea3f076c3d4b1f189e22
monicakornis <2011mkornis@gmail.com>
2018-01-05 20:24:37 -0500

    adding css changes

--- frontend/components/main_page/recipe_index_item.jsx
+++ frontend/components/main_page/recipe_index_item.jsx
@@ -5,6 +5,7 @@ const RecipeIndexItem = ({recipe}) => (
     <div className='col-recipe-index'>
       <Link to={`/recipes/${recipe.id}`}/>
       <img className="recipeIndexImage" src={recipe.image_url}/>
+      
       <div className='recipeInfo'>
         <h3>{recipe.title}</h3>
         <h4> By Monica Kornis </h4>
