971d2a16f07f540aa5feea13c40ffab93e4ecb85
monicakornis <2011mkornis@gmail.com>
2018-01-12 02:20:35 -0500

    adding css changes and reseeded with recipes

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,8 +15,18 @@ User.create!({email: 'MonicaKornis@gmail.com', password: 'password123', name: 'M
   User.create!(email: (Faker::Name.name).split(' ').join('') + "@gmail.com", password: 'password123', name: Faker::Name.name )
 end
 
-50.times do
-  recipe = Recipe.new(title: Faker::Food.dish,
+titles = ['','Cinnamon Rolls With Icing', 'Soy-Sauce-Pickled Eggs','Omelet With Sriracha Syrup',
+          'Lemony Egg Soup With Escarole', 'Coconut Chicken Curry', 'Dosas With Mustard Greens',
+          'Galette des Rois','Spinach Bouillabaisse', 'Garlic Chicken With Tomatoes', 'Omelets With Roasted Vegetables',
+          'Cold Sesame Noodles','Caramelized Citrus', 'Intense Chocolate Mousse Cake', 'Pistachio Whirligig Buns',
+          'Grilled Cheese Sandwich','Cocotte Burger','Post-Thanksgiving Cobb Salad', 'Salade Lyonnaise', 'Soba Salad', 'Bulgur and Tomato Casserole',
+          'Angel Food Cake With Plums', 'Panzanella With Winter Squash and Sage', 'Roasted Winter Squash With Cod', 'Cod Ceviche',
+          'Alaskan Cod in Sweet and Sour Pepper Sauce','Seared Grapefruit With Ginger Maple Syrup','Red Lentil Soup With Lemon', 'Cauliflower With Rosemary',
+          'Kale-Sauce Pasta','Panettone Bread Pudding', 'Cake With Marshmallow Frosting','Roasted Squash With Coconut']
+
+ (1..30).each do |num|
+   path = "./app/assets/images/#{num}.jpg"
+   recipe = Recipe.new(title: titles[num],
                 description: "Easy, healthful and infinitely adaptable, grain bowls are equally ideal for feeding a family (picky eaters can build it to suit their own tastes) and using up leftovers in the fridge. You could also cook up the ingredients at the beginning of the week, and mix and match ingredients at will as the week progresses",
                 author_id: User.find_by(email: 'MonicaKornis@gmail.com').id,
                 cooking_time: rand(32..59),
@@ -25,7 +35,7 @@ end
                 'Place one circle of dough on a baking sheet lined with parchment paper. Spread the filling evenly over the dough, leaving a 1-inch border bare. Press the charm into the filling. Moisten the border with cold water, position the second circle of dough over the filling and press around the border with your fingertips to seal well.',
                 'With the point of a paring knife, etch a design into the top of the galette, taking care not to pierce the dough. Cut 6 small slits in the top as steam vents.'],
                 ingredients: ['5/6 cup ' + Faker::Food.ingredient,'1/4 cup ' + Faker::Food.ingredient,  '1/2 tbs ' + Faker::Food.spice, '1/2 cup ' + Faker::Food.ingredient, '1/2 cup ' + Faker::Food.ingredient, '3/4 tbs ' + Faker::Food.spice])
-                file=File.open('./app/assets/images/toast.jpg')
+                file=File.open(path)
                 recipe.image = file
                 recipe.save!
 end
