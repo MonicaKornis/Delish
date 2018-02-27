b8eda0cdea97654791a5cc12b4a1f68f0bb105c6
monicakornis <2011mkornis@gmail.com>
2018-02-27 11:33:53 -0500

    made minor changes to package json

--- webpack.config.js
+++ webpack.config.js
@@ -24,7 +24,7 @@ plugins = plugins.concat(
 // include plugins config
 module.exports = {
   context: __dirname,
-  entry: "./frontend/<name of entry file>",
+  entry: "./frontend/index.jsx",
   output: {
     path: path.resolve(__dirname, "app", "assets", "javascripts"),
     filename: "bundle.js"
